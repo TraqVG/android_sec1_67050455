3c0fefa592ce38ca4e4e93c3b1b2f7a4c50f1d78
Traqvg <traqvg@gmail.com>
2026-07-16 18:50:12 +0700

    Lesson3

---
+++ Lesson3_ClassesAndObjects/src/Main.kt
@@ -0,0 +1,27 @@
+fun buildAquarium() {
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
+}
+
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
+fun main() {
+    buildAquarium()
+    makeFish()
+    makeDecorations()
+
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+}
